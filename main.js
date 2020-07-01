16e0573b21b22334b4fd436fa5249a1b2fb99252
Colin Jaffe <balloonasaurus@gmail.com>
2020-07-01 08:49:21 -0400

    Adds not-defined safety.

--- main.js
+++ main.js
@@ -128,6 +128,34 @@ const add1ToLeft = function(numbers) {
 }
 
 
+if (typeof doubleAll === 'undefined') {
+  doubleAll = undefined;
+}
+
+if (typeof yelledGreetings === 'undefined') {
+  yelledGreetings = undefined;
+}
+
+if (typeof absoluteValues === 'undefined') {
+  absoluteValues = undefined;
+}
+
+if (typeof upperCaseFirstLetters === 'undefined') {
+  upperCaseFirstLetters = undefined;
+}
+if (typeof changeToInitials === 'undefined') {
+  changeToInitials = undefined;
+}
+
+if (typeof doubleOdd === 'undefined') {
+  doubleOdd = undefined;
+}
+
+if (typeof add1ToLeft === 'undefined') {
+  add1ToLeft = undefined;
+}
+
+
 module.exports = {
   doubleAll,
   yelledGreetings,
